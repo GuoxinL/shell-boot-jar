455623f86c9798300f62c9e2a68750e24ce50483
Guoxin Liu <lgx31@sina.cn>
2018-11-26 11:09:51 +0800

    > /dev/null 2>&1
    > /dev/null 2>&1

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@
 #               Start service
 #
 
-nohup java -jar birdnest-api-booter.jar --spring.profiles.active=$1 &
+nohup java -jar birdnest-api-booter.jar --spring.profiles.active=$1 > /dev/null 2>&1 &
 echo $! > main.pid
